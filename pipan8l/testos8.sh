b7f149359b58bf6db7bb584e6dae15c387eaedd9
mrieker@nii.net <mrieker@nii.net>
2024-11-12 17:06:26 +0000

    get testos8 working
    runs with z8llib_enlo4k=0 or z8llib_enlo4k=1

--- pipan8l/testos8.sh
+++ pipan8l/testos8.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+function runtest {
+    rm -f /tmp/testos8_rk8je_pipe
+    ./z8lsim testos8.tcl
+    exst=$?
+    echo exit >&200
+    return $exst
+}
 echo = = = = = = = =
 echo loading disk
 rm -f /tmp/testos8_rk8je_pipe
@@ -6,12 +13,11 @@ mkfifo /tmp/testos8_rk8je_pipe
 cp rkab0.rk05 ~/rkab0.rk05
 ./z8lrk8je -loadrw 0 ~/rkab0.rk05 < /tmp/testos8_rk8je_pipe &
 rkpid=$!
-(
-    rm -f /tmp/testos8_rk8je_pipe
-    sleep 1
-    echo = = = = = = = =
-    echo testing
-    ./z8lsim testos8.tcl
-    echo = = = = = = = =
-) 200>/tmp/testos8_rk8je_pipe
+echo = = = = = = = =
+echo testing
+runtest 200>/tmp/testos8_rk8je_pipe
+exst=$?
+echo = = = = = = = =
+echo exiting
 wait $rkpid
+exit $exst
--- pipan8l/testos8.tcl
+++ pipan8l/testos8.tcl
@@ -6,91 +6,9 @@ openttypipes
 
 startat [loadbin rk8jeboot.bin]
 
-# check tty output for match
-# always ignore control characters and whitespace
-# skip up to nskip printables at the beginning
-# return string skipped
-proc checkttymatch {nskip line {tmsec 30000}} {
-    set len [string length $line]
-    set nowtsp ""
-    for {set i 0} {$i < $len} {incr i} {
-        set ch [string index $line $i]
-        if {$ch > " "} {append nowtsp $ch}
-    }
-    set len [string length $nowtsp]
-    set nmatched 0
-    set skipped ""
-    while {$nmatched < $len} {
-        set ch [readttychartimed $tmsec]
-        if {$ch == ""} {
-            puts ""
-            puts "checkttymatch: timed out waiting for '$nowtsp'"
-            exit 1
-        }
-        puts -nonewline $ch
-        flush stdout
-        if {$ch <= " "} continue
-        if {$ch == [string index $nowtsp $nmatched]} {
-            incr nmatched
-            continue
-        }
-        append skipped [string range $nowtsp 0 [expr {$nmatched - 1}]]
-        append skipped $ch
-        if {[string length $skipped] > $nskip} {
-            puts ""
-            puts "checkttymatch: failed to match '$nowtsp'"
-            exit 1
-        }
-    }
-    return $skipped
-}
+puts "testos8: starting"
 
-# check tty output for match
-# always ignore control characters
-# skip up to nskip printables at the beginning
-# return string skipped
-proc checkttyprompt {prompt reply} {
-    # check prompt string
-    checkttymatch 0 $prompt
-    # there may be a CR/LF following the prompt (like 'READY' in BASIC)
-    # so wait for a brief time with no characters printed
-    while {true} {
-        set rb [readttychartimed 1234]
-        if {$rb == ""} break
-        puts -nonewline $rb
-        flush stdout
-        if {$rb > " "} {
-            puts checkttyprompt: extra char [escapechr $rb] after prompt"
-        }
-    }
-    # send reply string to keyboard incl spaces
-    # check for each of each one
-    global wrkbpipe
-    set len [string length $reply]
-    for {set i 0} {$i < $len} {incr i} {
-        after 456
-        set ch [string index $reply $i]
-        puts -nonewline $wrkbpipe $ch
-        chan flush $wrkbpipe
-        set rb [readttychartimed 1234]
-        puts -nonewline $rb
-        flush stdout
-        if {$rb != $ch} {
-            puts ""
-            puts "checkttyprompt: char [escapechr $ch] echoed as [escapechr $rb]"
-            exit 1
-        }
-    }
-    # send CR at the end, don't bother checking echo in case it echoes something different
-    after 1234
-    puts -nonewline $wrkbpipe "\r"
-    chan flush $wrkbpipe
-}
-
-puts "testos8*: starting"
-
-checkttymatch 0 "PiDP-8/I trunk:id\[76133aacd0\] - OS/8 V3D Combined Kit - KBM V3T - CCL V3"
-checkttymatch 0 "A"
+checkttymatch 0 "PiDP-8/I trunk:id\[76133aacd0\] - OS/8 V3D Combined Kit - KBM V3T - CCL V3A"
 checkttymatch 0 "Built from source by tangent@pidp8i on 2021.02.14 at 15:37:06 MST"
 checkttymatch 0 "Restart address = 07600"
 checkttymatch 0 "Type:"
@@ -113,17 +31,39 @@ checkttymatch 0 "DON'T START PLAYING WITH LESS THAN \$100.. HAVE FUN!"
 checkttyprompt "HOW MANY DOLLARS ARE YOU STARTING WITH?" "500"
 checkttyprompt "WHAT IS YOUR WAGER THIS TIME?" "100"
 checkttymatch 0 "YOUR FIRST CARD IS "
-set firstcard [string trim [checkttymatch 5 "YOUR SECOND CARD IS "]]
-set secondcard [string trim [checkttymatch 5 "DEALER SHOWS A "]]
-set dealershows [string trim [checkttymatch 5 "YOU HAVE"]]
+set firstcard [string trim [getrestofttyline]]
+checkttymatch 0 "YOUR SECOND CARD IS "
+set secondcard [string trim [getrestofttyline]]
+checkttymatch 0 "DEALER SHOWS A "
+set dealershows [string trim [getrestofttyline]]
+checkttymatch 0 "YOU HAVE"
 set mytotal [string trim [checkttymatch 5 "SHOWING."]]
 checkttyprompt "DO YOU WANT A HIT??" "N"
-checkttymatch 0 "DEALER HAS  "
-set dealerhas [string trim [checkttymatch 5 "YOU"]]
-checkttymatch 0 "LOSE. YOU NOW HAVE \$ 400 "
+while true {
+    checkttymatch 0 "DEALER HAS  "
+    set dealerhas [string trim [getrestofttyline]]
+    if {$dealerhas > 21} {
+        checkttymatch 0 "DEALER BUSTED."
+        break
+    }
+    if {$dealerhas >= 17} break
+    checkttymatch 0 "DEALER GETS A "
+    set dealergets [string trim [getrestofttyline]]
+}
+set ilost [expr {($dealerhas >= $mytotal) && ($dealerhas <= 21)}]
+if {$ilost} {
+    checkttymatch 0 "YOU LOSE. YOU NOW HAVE \$ 400 "
+} else {
+    checkttymatch 0 "YOU WIN. YOU NOW HAVE \$ 600 "
+}
 checkttyprompt "DO YOU WISH TO PLAY AGAIN??" "N"
-checkttymatch 0 "TOO BAD! YOU LOST 100 DOLLARS AT THE EDUSYSTEM CASINO."
+if {$ilost} {
+    checkttymatch 0 "TOO BAD! YOU LOST 100 DOLLARS AT THE EDUSYSTEM CASINO."
+} else {
+    checkttymatch 0 "NOT BAD! YOU WON 100 DOLLARS AT THE EDUSYSTEM CASINO."
+}
 checkttymatch 0 "HOPE YOU ENJOYED YOURSELF.  THANKS FOR PLAYING."
 checkttyprompt "READY" "BYE"
 checkttymatch 0 "."
+puts ""
 exit
