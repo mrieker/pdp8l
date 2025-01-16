c15246845c2140913513e63e3547413123b46bb4
mrieker@nii.net <mrieker@nii.net>
2025-01-16 14:47:02 -0500

    add hardreset command to z8lpanelini.tcl
    will unstick a hung PDP
      run light is on
      but it is not cycling

--- pipan8l/z8lpanelini.tcl
+++ pipan8l/z8lpanelini.tcl
@@ -1,2 +1,23 @@
 ;# get regular pipan8l init file
-return [source "[file dirname [info script]]/pipan8lini.tcl"]
+set oldhelp [source "[file dirname [info script]]/pipan8lini.tcl"]
+
+rename helpini old_helpini
+
+# if the PDP gets jammed, try this to unstick it
+proc hardreset {} {
+    setsw step 1
+    pin set bareit 1 x_MEM 0 i_MEMDONE 0
+    pin set bareit 1 x_MEM 0 i_MEMDONE 1
+    pin set bareit 1 x_MEM 0 i_STROBE 0
+    pin set bareit 1 x_MEM 0 i_STROBE 1
+    pin set bareit 0 x_MEM 1 i_STROBE 1
+    stopandreset
+}
+
+proc helpini {} {
+    old_helpini
+    puts "  hardreset  - unjam a jammed PDP"
+    puts ""
+}
+
+return $oldhelp
