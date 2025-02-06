b0ccfb0874159066dfe90a00844275ab70e09eec
mrieker@nii.net <mrieker@nii.net>
2025-02-06 08:03:49 -0500

    have z8lpanelini.tcl turn on fpga clocking on startup and hardreset

--- pipan8l/z8lpanelini.tcl
+++ pipan8l/z8lpanelini.tcl
@@ -1,21 +1,28 @@
-;# get regular pipan8l init file
-set oldhelp [source "[file dirname [info script]]/pipan8lini.tcl"]
+#
+# initialization file for z8lpanel
+#
 
+# get regular pipan8l init file
+set oldhelp [source "[file dirname [info script]]/pipan8lini.tcl"]
 rename helpini old_helpini
+
+# make sure fpga clocking is turned on
+pin set nanocontin 1
 
 # if the PDP gets jammed, try this to unstick it
 proc hardreset {} {
-    set enlo4k [pin get xm_enlo4k]
-    set simit [pin get simit]
-    setsw step 1
-    pin set fpgareset 1 fpgareset 0
-    pin set simit $simit xm_enlo4k $enlo4k
+    set enlo4k [pin get xm_enlo4k]                  ;# save enlo4k
+    set simit [pin get simit]                       ;# save simit
+    setsw step 1                                    ;# tell processor to stop asap
+    pin set nanocontin 1 fpgareset 1 fpgareset 0    ;# turn on fpga clocking, pulse fpga reset
+    pin set simit $simit xm_enlo4k $enlo4k          ;# restore simit, enlo4k
     if {! $simit} {
+        ;# real PDP, pulse i_STROBE and i_MEMDONE lines
         pin set bareit 1 r_MA 1 x_MEM 0 i_MEMDONE 0 i_MEMDONE 1 x_MEM 1
         pin set bareit 1 r_MA 1 x_MEM 0 i_STROBE 0  i_STROBE 1  x_MEM 1
         pin set bareit 0
     }
-    stopandreset
+    stopandreset                                    ;# make sure stopped and cleared
 }
 
 proc helpini {} {
