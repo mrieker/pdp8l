6c5b98d96b7d61dc45a7216cf476bc1c4f84f93a
mrieker@nii.net <mrieker@nii.net>
2025-02-16 19:04:40 -0500

    add comments to timemem.tcl (memory timing script)

--- pipan8l/timemem.tcl
+++ pipan8l/timemem.tcl
@@ -3,15 +3,23 @@
 # Prints cycle length: count
 #  length is in 10nS units
 #
-proc timemem {} {
+# $ ./z8lpanel
+# z8lpanel> source timemem.tcl
+# z8lpanel> timemem
+#
+proc timemem {{nloop 10000}} {
     set counts [dict create]
-    for {set i 0} {($i < 10000) & ! [ctrlcflag]} {incr i} {
+    for {set i 0} {($i < $nloop) & ! [ctrlcflag]} {incr i} {
+        # collect 6 samples
         pin set xm_memcycctrhi 0
         while true {
             set hi [pin get xm_memcycctrhi]
             if {$hi >= 0x60000000} break
         }
         set lo [pin get xm_memcycctrlo]
+        # add counts to dictionary
+        # fpga discards oldest cuz it's probably a runt cuz probably
+        #  cleared memcycctrhi somewhere in middle of cycle
         for {set j 0} {$j < 5} {incr j} {
             set ctr [expr {$lo & 0x3FF}]
             dict incr counts $ctr
@@ -19,6 +27,7 @@ proc timemem {} {
             set hi [expr {$hi >> 12}]
         }
     }
+    # print dictionary
     foreach key [dict keys $counts] {
         puts "$key: [dict get $counts $key]"
     }
